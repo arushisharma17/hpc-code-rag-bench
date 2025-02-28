b0c4ffd600bcf2f58494106d6fa55d4183247bdd
adis azhar <adisazhar123@gmail.com>
2025-02-28 17:30:43 -0600

    parameterize generation without retrieval

--- run_generation_without_retrieval.sh
+++ run_generation_without_retrieval.sh
@@ -17,22 +17,35 @@
 #SBATCH --gres=gpu:a100:1   # Required GPU hardware
 
 
+PROJECT_DIR=${PROJECT_DIR:-"/work/classtmp/azhar/projects/hpc-code-rag-bench"}
+MICROMAMBA_ROOT_PATH=${MICROMAMBA_ROOT_PATH:-"/work/classtmp/azhar/micromamba"}
+MICROMAMBA_ENV_NAME=${MICROMAMBA_ENV_NAME:-"coderag"}
+MICROMAMBA_ENV_PATH="${MICROMAMBA_ROOT_PATH}/envs/${MICROMAMBA_ENV_NAME}"
+HF_HOME_PATH=${HF_HOME_PATH:-"/work/classtmp/azhar/hf"}
+MODEL=${MODEL:-"bigcode/starcoder2-7b"}
+DATASET_PATH=${DATASET_PATH:-"openai_humaneval"}
+TASKS=${TASKS:-"humaneval"}
+
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 module purge
 module load micromamba
 module load git
 module load cuda
 
-cd /work/classtmp/azhar/projects/hpc-code-rag-bench/generation
+cd "$PROJECT_DIR/generation"
 
-micromamba activate coderag
+echo "PROJECT_DIR is $PROJECT_DIR, MICROMAMBA_ROOT_PATH is $MICROMAMBA_ROOT_PATH, MICROMAMBA_ENV_NAME is $MICROMAMBA_ENV_NAME, MICROMAMBA_ENV_PATH is $MICROMAMBA_ENV_PATH, HF_HOME_PATH is $HF_HOME_PATH, MODEL is $MODEL, DATASET_PATH is $DATASET_PATH, TASKS is $TASKS"
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba activate "$MICROMAMBA_ENV_PATH"
 
 echo "running generation..."
 
 # generation without retrieval
-HF_HOME=/work/classtmp/azhar/hf OPENAI_API_KEY=dummy-for-now API_KEY=dummy-for-now python main.py --tasks "humaneval" \
---model "bigcode/starcoder2-7b" \
---dataset_path "openai_humaneval" \
+HF_HOME="$HF_HOME_PATH" OPENAI_API_KEY=dummy-for-now API_KEY=dummy-for-now python main.py \
+--tasks "$TASKS" \
+--model "$MODEL" \
+--dataset_path "$DATASET_PATH" \
 --allow_code_execution
 
 
