1c033d3d5a826babf6e99483226078353faedf6f
adis azhar <adisazhar123@gmail.com>
2025-03-17 01:25:12 -0500

    add sbatch scripts

---
+++ sbatch-scripts/codegen_github_llama.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#SBATCH --time=10:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1            # Number of nodes requested (1 nodes)
+#SBATCH --ntasks-per-node=36   # 36 processor core(s) per node
+#SBATCH --job-name="codegen_github_llama"
+#SBATCH --mail-user=azhar@iastate.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=instruction       # class node(s)
+#SBATCH --account=s2025.coms.599.3    # account to use
+#SBATCH --gres=gpu:a100:1   # Required GPU hardware
+
+
+PROJECT_DIR=${PROJECT_DIR:-"/work/classtmp/azhar/projects/hpc-code-rag-bench"}
+MICROMAMBA_ROOT_PATH=${MICROMAMBA_ROOT_PATH:-"/work/classtmp/azhar/micromamba"}
+MICROMAMBA_ENV_NAME=${MICROMAMBA_ENV_NAME:-"hpccoderag"}
+MICROMAMBA_ENV_PATH="${MICROMAMBA_ROOT_PATH}/envs/${MICROMAMBA_ENV_NAME}"
+HF_HOME_PATH=${HF_HOME_PATH:-"/work/classtmp/azhar/hf"}
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load micromamba
+module load git
+module load cuda
+
+echo "PROJECT_DIR is $PROJECT_DIR, MICROMAMBA_ROOT_PATH is $MICROMAMBA_ROOT_PATH, MICROMAMBA_ENV_NAME is $MICROMAMBA_ENV_NAME, MICROMAMBA_ENV_PATH is $MICROMAMBA_ENV_PATH, HF_HOME_PATH is $HF_HOME_PATH"
+cd "$PROJECT_DIR/PerfOpt/Evaluation" || exit
+echo "current workdir is $(pwd)"
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba activate "$MICROMAMBA_ENV_PATH"
+
+models=(
+  "meta-llama/Llama-3.1-8B-Instruct"
+  "meta-llama/Llama-3.2-1B-Instruct"
+  "meta-llama/Llama-3.2-3B-Instruct"
+)
+retrieval_paths=(
+  "datasets/github/GIST-Embedding-v0.json"
+  "datasets/github/st-codesearch-distilroberta-base.json"
+)
+corpus_path="datasets/github/corpus.jsonl"
+
+for model in "${models[@]}"; do
+    for retrieval in "${retrieval_paths[@]}"; do
+        echo "Starting evaluation for model: $model with retrieval path: $retrieval"
+
+        python evals.py \
+            --dataset_type code_generation \
+            --data_file queries.jsonl \
+            --model_names "$model" \
+            --prompt_type standard \
+            --eval_type codebertscore \
+            --rag \
+            --k_documents 3 \
+            --retrieval_path "$retrieval" \
+            --corpus_path "$corpus_path"
+
+        echo "Finished evaluation for model: $model with retrieval path: $retrieval"
+        echo "----------------------------------------"
+    done
+done
---
+++ sbatch-scripts/setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copy/paste this job script into a text file and submit with the command:
+#    sbatch thefilename
+# job standard output will go to the file slurm-%j.out (where %j is the job ID)
+
+#SBATCH --time=2:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=36   # 36 processor core(s) per node
+#SBATCH --job-name="setup"
+#SBATCH --mail-user=azhar@iastate.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=instruction       # class node(s)
+#SBATCH --account=s2025.coms.599.3    # account to use
+
+# Parameterized variables
+PROJECT_DIR=${PROJECT_DIR:-"/work/classtmp/azhar/projects/hpc-code-rag-bench"}
+MICROMAMBA_ROOT_PATH=${MICROMAMBA_ROOT_PATH:-"/work/classtmp/azhar/micromamba"}
+MICROMAMBA_ENV_NAME=${MICROMAMBA_ENV_NAME:-"hpccoderag"}
+MICROMAMBA_ENV_PATH="${MICROMAMBA_ROOT_PATH}/envs/${MICROMAMBA_ENV_NAME}"
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load micromamba
+module load git
+module load cuda
+
+echo "setting up $MICROMAMBA_ENV_NAME"
+echo "PROJECT_DIR is $PROJECT_DIR, MICROMAMBA_ROOT_PATH is $MICROMAMBA_ROOT_PATH, MICROMAMBA_ENV_NAME is $MICROMAMBA_ENV_NAME, MICROMAMBA_ENV_PATH is $MICROMAMBA_ENV_PATH"
+
+cd "$PROJECT_DIR/PerfOpt" || exit
+echo "current workdir is $(pwd)"
+
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba env create -n "$MICROMAMBA_ENV_NAME" python=3.10.16 -c conda-forge -y -r "$MICROMAMBA_ROOT_PATH"
+micromamba activate "$MICROMAMBA_ENV_PATH"
+
+echo "$MICROMAMBA_ENV_NAME created"
+
+if micromamba activate "$MICROMAMBA_ENV_PATH"; then
+    echo "Environment $MICROMAMBA_ENV_NAME activated successfully."
+    python -m pip install -r requirements.txt
+
+    # List installed packages in the environment
+    echo "Listing installed packages in $MICROMAMBA_ENV_NAME:"
+    micromamba list
+else
+    echo "Failed to activate environment $MICROMAMBA_ENV_NAME."
+    exit 1
+fi
