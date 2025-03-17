20a553d292b1f7453cb99a1ac10b6c69777cb644
adis azhar <adisazhar123@gmail.com>
2025-03-17 01:24:35 -0500

    add more models

--- PerfOpt/Evaluation/pipeline_code_generation.py
+++ PerfOpt/Evaluation/pipeline_code_generation.py
@@ -187,7 +187,54 @@ def llm_generate_llama(model_name, question, parameters):
     return response
 
 
+def llm_generate_deepseek_coder(model_name, question, parameters):
+    model_kwargs = {
+        'device_map': 'auto'
+    }
+    if parameters['load_in_4bit']:
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        model_kwargs['quantization_config'] = quantization_config
+    else:
+        model_kwargs['torch_dtype'] = 'auto'
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        **model_kwargs
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    messages = [
+        {"role": "system",
+         "content": " You are an expert in writing performant OpenMP code. You need to write code following the given function signature. Do not include any explanations."},
+        {"role": "user", "content": question}
+    ]
+
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+    default_params = CONFIG['code_generation']['default_parameters']['deepseek-coder']
+
+    generated_ids = model.generate(
+        **model_inputs,
+        **default_params,
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=False)[0]
+    return response
+
+
 def code_generation(model, question, parameters):
+    if model in CONFIG['code_generation']['models'] and model.startswith('deepseek-ai/deepseek-coder'):
+        return llm_generate_deepseek_coder(model, question, parameters)
     if model in CONFIG['code_generation']['models'] and model.startswith('Qwen/Qwen2.5-Coder'):
         return llm_generate_qwen_coder(model, question, parameters)
     if model in CONFIG['code_generation']['models'] and model.startswith('HuggingFaceTB/SmolLM2'):
